9cd85853e4d6245947ab0ebb9b0338ffe1a57d28
Maria Martinkous <marticaida@gmail.com>
2022-07-20 20:43:22 -0500

    fixed user input

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 window.onload = function () {
+    // set default game size
+    let gridSide = 16;
+
+    // input button space
+    const headerContainer = document.createElement('div');
+    document.body.appendChild(headerContainer);
+    headerContainer.style.width = "800px";
+    headerContainer.style.height = "50px";
+    headerContainer.style.backgroundColor = "pink";
+
+    //  create input button
+    const inputBtn = document.createElement('button');
+    inputBtn.textContent = "Make your own field";
+    headerContainer.appendChild(inputBtn);
+    inputBtn.addEventListener("click", popUp);
+    function popUp() {
+        gridSide = prompt("Type a number from 10 to 100");
+        if (gridSide < 10 || gridSide > 100){
+            alert ("Type a number from 10 to 100");
+            gridSide = prompt("Type a number from 10 to 100");
+            return gridSide;
+     } else if (gridSide != null) {
+            return gridSide;
+        };
+        console.log(gridSide);
+    };
+    gridSide = popUp();
+
+    // create the game field
     const gridContainer = document.createElement('div');
-    for (let i = 1; i <= 256; i++) {
+    // make squares 
+
+    for (let i = 1; i <= (gridSide*gridSide); i++) {
+        console.log(i)
         const block = document.createElement('div');
         block.style.backgroundColor = "teal";
         gridContainer.appendChild(block);
-        block.addEventListener("mouseenter", changeColor);
-        function changeColor() {
-            block.style.backgroundColor = "black";
-        };
+        block.setAttribute('id', 'block');
+        // adds hover effect to draw with the mouse 
+        // block.addEventListener("mouseenter", changeColor);
+        // function changeColor() {
+        //     block.style.backgroundColor = "black";
+        // };
     };
+
     document.body.appendChild(gridContainer);
+    gridContainer.setAttribute('id', 'container');
     gridContainer.style.width = "800px";
     gridContainer.style.height = "800px";
     gridContainer.style.display = "grid";
-    gridContainer.style.gridTemplateColumns = "repeat(16, auto)";
-    gridContainer.style.gridTemplateRows = "repeat(16, auto)";
-    // gridContainer.style.gap = "5px";
-    // gridContainer.style.alignItems = "center";
-    // gridContainer.style.justifyContent = "center";
-    // gridContainer.style.alignContent = "center";
-
+    gridContainer.style.gridTemplateColumns = "repeat(gridSide, auto)";
+    gridContainer.style.gridTemplateRows = "repeat(gridSide, auto)";
 };
